2433db727a404e54d3bb5c9639113e337cf22cf2
amatsagu <krzysztof.rapior@protonmail.com>
2025-11-16 13:02:38 +0100

    feat: add basic settings struct

---
+++ src/main/java/amatsagu/merito/Settings.java
@@ -0,0 +1,11 @@
+package amatsagu.merito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Settings {
+    GameMode gameMode;
+    int minRange;
+    int maxRange;
+    List<String> playerNames = new ArrayList<>();
+}
